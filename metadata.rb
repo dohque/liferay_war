a201b74ff356f8293448889920110af5e6202850
Ruslan Pilin <dohque@gmail.com>
2013-01-15 18:45:24 +0400

    Creating skeleton for cookbook

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "PilinSolutions"
+maintainer_email "dohque@gmail.com"
+license          ""
+description      "Installs/Configures liferay_war"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: liferay_war
+# Recipe:: default
+#
+# Copyright 2013, PilinSolutions
+#
